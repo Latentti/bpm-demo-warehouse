46c1d8632c0c411a7536e715f11cd8a542ac8018
Jani Sundqvist <jani@mustakotelo.fi>
2021-05-17 16:59:30 +0300

    Fixed try catch and improved spacing.

--- handlers/deploy.js
+++ handlers/deploy.js
@@ -5,29 +5,30 @@ const FormData = require('form-data');
 const { log } = require('../utils/logger')
 
 module.exports = async ( props ) => {
-  const diagrams = fs.readdirSync('./diagrams/');
-
-  const diagramRequests = diagrams.map((diagram) => {
-    let formData = new FormData()
-    formData.append('deployment-name', diagram)
-    formData.append('deployment-source', process.env.CAMUNDA_WORKER_NAME)
-    formData.append('enable-duplicate-filtering', "true")
-    formData.append('deploy-changed-only', "true")
-    formData.append('upload', fs.createReadStream(`./diagrams/${diagram}/${diagram}.bpmn`))
-
-    return axios.post(`${process.env.BASEURL}/deployment/create`, formData, {
-      auth: {
-        username: process.env.CAMUNDA_USERNAME || "demo",
-        password: process.env.CAMUNDA_PASSWORD || "demo"
-      },
-      headers: {
-        ...formData.getHeaders(),
-      },
-    })
-  })
-
   try{
+    const diagrams = fs.readdirSync('./diagrams/');
+
+    const diagramRequests = diagrams.map((diagram) => {
+      let formData = new FormData()
+      formData.append('deployment-name', diagram)
+      formData.append('deployment-source', process.env.CAMUNDA_WORKER_NAME)
+      formData.append('enable-duplicate-filtering', "true")
+      formData.append('deploy-changed-only', "true")
+      formData.append('upload', fs.createReadStream(`./diagrams/${diagram}/${diagram}.bpmn`))
+
+      return axios.post(`${process.env.BASEURL}/deployment/create`, formData, {
+        auth: {
+          username: process.env.CAMUNDA_USERNAME || "demo",
+          password: process.env.CAMUNDA_PASSWORD || "demo"
+        },
+        headers: {
+          ...formData.getHeaders(),
+        },
+      })
+    })
+
     const responses = await Promise.all(diagramRequests)
+
     responses.forEach((diagram) => {
       log(`Deployed ${diagram.data.name} [${diagram.data.id}]`)
     })
