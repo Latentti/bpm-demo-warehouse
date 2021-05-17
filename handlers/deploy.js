6aeb5bf0dd4513ab48c66424ee470abcf6bf4d82
Jani Sundqvist <jani@mustakotelo.fi>
2021-05-17 16:53:17 +0300

    Deployment handler

---
+++ handlers/deploy.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const axios = require('axios');
+const FormData = require('form-data');
+
+const { log } = require('../utils/logger')
+
+module.exports = async ( props ) => {
+  const diagrams = fs.readdirSync('./diagrams/');
+
+  const diagramRequests = diagrams.map((diagram) => {
+    let formData = new FormData()
+    formData.append('deployment-name', diagram)
+    formData.append('deployment-source', process.env.CAMUNDA_WORKER_NAME)
+    formData.append('upload', fs.createReadStream(`./diagrams/${diagram}/${diagram}.bpmn`))
+
+    return axios.post(`${process.env.BASEURL}/deployment/create`, formData, {
+      auth: {
+        username: process.env.CAMUNDA_USERNAME || "demo",
+        password: process.env.CAMUNDA_PASSWORD || "demo"
+      },
+      headers: {
+        ...formData.getHeaders(),
+      },
+    })
+  })
+
+  try{
+    const responses = await Promise.all(diagramRequests)
+    responses.forEach((diagram) => {
+      log(`Deployed ${diagram.data.name} [${diagram.data.id}]`)
+    })
+  }catch(ex){
+    log(`Exception while deploying processes: ${ex.message}`, 3)
+    console.log("Exception stack", ex)
+  }
+
+
+}
