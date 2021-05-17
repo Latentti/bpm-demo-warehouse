51606baedf5814aa7488a4568ff39ab65968b7e4
Jani Sundqvist <jani@mustakotelo.fi>
2021-05-17 10:43:48 +0300

    Created handlers, fixed diagram hierarchy

---
+++ handlers/subscribe.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+const { log } = require('../utils/logger')
+const errorHandler = require('./error')
+
+module.exports = async ( client ) => {
+  const modules = fs.readdirSync('./modules/').map((mod) => `./modules/${mod}`);
+  modules.forEach((mod) => {
+    const tasks = fs.readdirSync(mod).filter(file => file.endsWith(".js"));
+    tasks.forEach(task => {
+      const name = task.slice(0, -3);
+      log(`Loading topic ${name}`)
+      client.subscribe(name, async (props) => {
+        try{
+          log(`Received '${props.task.topicName}' on '${props.task.processDefinitionKey}' [${props.task.processInstanceId}]`)
+          await require(`${mod}/${task}`)( props, client, log )
+        }catch(ex){
+          await errorHandler( props, client, ex )
+        }
+      })
+    })
+  })
+}
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
 
-const fs = require('fs');
 const { Client, logger } = require('camunda-external-task-client-js');
-const modules = fs.readdirSync('./modules/').map((mod) => `./modules/${mod}`);
+
+const subscribeHandler = require('./handlers/subscribe')
 const start = Date.now()
 
 const { log } = require('./utils/logger')
-const errorHandler = require('./utils/errorHandler')
 
 log(`Starting camunda external service`)
 
@@ -18,20 +17,6 @@ const camundaConfig = {
 
 const client = new Client(camundaConfig);
 
-modules.forEach((mod) => {
-  const tasks = fs.readdirSync(mod).filter(file => file.endsWith(".js"));
-  tasks.forEach(task => {
-    const name = task.slice(0, -3);
-    log(`Loading topic ${name}`)
-    client.subscribe(name, async (props) => {
-      try{
-        log(`Received '${props.task.topicName}' on '${props.task.processDefinitionKey}' [${props.task.processInstanceId}]`)
-        await require(`${mod}/${task}`)( props, client, log )
-      }catch(ex){
-        await errorHandler( props, client, ex )
-      }
-    })
-  })
-})
+subscribeHandler(client)
 
 log(`Service loaded in ${Date.now() - start}ms`)
